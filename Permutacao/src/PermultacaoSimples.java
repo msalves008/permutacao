bfd6e92f77eb86bc330b67e53b7529128450789a
msalves008 <matheusdesousaalves2018@gmail.com>
2019-09-29 11:52:21 -0300

    IMPLEMENTANDO PERMUTAÇÃO SIMPLES

--- Permutacao/src/PermultacaoSimples.java
+++ Permutacao/src/PermultacaoSimples.java
@@ -1,4 +1,29 @@
 
 public class PermultacaoSimples {
 
+	public static void main(String[] args) {
+		
+	
+	double x = 69; // aqui criamos uma variável que irá armazenar o numero do fatorial
+	double f = x; // aqui criamos outra var. Será o resultado temporário da multiplicação
+
+		                  
+
+	
+
+	while (x > 1){
+	  f = f *(x-1); x--;
+	                    }
+	  System.out.println(f); // Aqui exibira o valor final do Fatorial
+
+	 while (x > 1){ 
+	             f = f *(x-1);
+	             x--;
+	             System.out.println(f); // Aqui exibira o valor parcial do Fatorial, o seu último valor será o resultado do fatorial.               }
+	
+	
+	
+}
+	  
+}
 }
