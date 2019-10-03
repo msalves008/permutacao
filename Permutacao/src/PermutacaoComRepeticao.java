dccdf5681ab031e3383967e6dfcd26dcbfae254b
msalves008 <matheusdesousaalves2018@gmail.com>
2019-10-03 06:55:43 -0300

    PERMUTAÇÃO COM REPATIÇÃO

--- Permutacao/src/PermutacaoComRepeticao.java
+++ Permutacao/src/PermutacaoComRepeticao.java
@@ -1,4 +1,77 @@
 
 public class PermutacaoComRepeticao {
-
+ private double nfatorial, rep1, rep2, rep3, rep4;
+ 
+ 
+	 public double getNfatorial() {
+		return nfatorial;
+	}
+	
+	
+	public void setNfatorial(double nfatorial) {
+		this.nfatorial = nfatorial;
+	}
+	
+	
+	public double getRep1() {
+		return rep1;
+	}
+	
+	
+	public void setRep1(double rep1) {
+		this.rep1 = rep1;
+	}
+	
+	
+	public double getRep2() {
+		return rep2;
+	}
+	
+	
+	public void setRep2(double rep2) {
+		this.rep2 = rep2;
+	}
+	
+	
+	public double getRep3() {
+		return rep3;
+	}
+	
+	
+	public void setRep3(double rep3) {
+		this.rep3 = rep3;
+	}
+	
+	
+	public double getRep4() {
+		return rep4;
+	}
+	
+	
+	public void setRep4(double rep4) {
+		this.rep4 = rep4;
+	}
+			
+	
+		double f;
+			
+			public void fatorial(double n) {
+			double f;
+				f =  n; 
+			
+					while ( n > 1){
+					  f = f *( n-1);  n--;
+					}
+			}
+			
+			public double permutacao() {
+				 double result;
+				 result  =  f;
+				 
+				 
+				 return result;
+			 }
+	 
+ 
+ 
 }
--- Permutacao/src/Principal.java
+++ Permutacao/src/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		PermultacaoSimples ps = new PermultacaoSimples();
+		PermutacaoComRepeticao  pr =  new PermutacaoComRepeticao();
+		
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("-------------------------------------------------\n BEM VINDO!!!! \n-------------------------------------------------");
 		
